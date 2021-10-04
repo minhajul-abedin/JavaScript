6237c573935aa90930c811462ab9c3933475fcd7
Minhaj <mdtoyon37@gmail.com>
2021-10-04 15:15:11 +0600

    vaiable

--- 1st/variable.js
+++ 1st/variable.js
@@ -1,7 +1,17 @@
 
+var name= 'Minhajul Abedin'
+var age= '23'
 
+// without variable
 
 console.log ('Minhajul Abedin knows Html Css')
 console.log ('His age is 23')
 console.log ('He want to learn JavaScript')
 console.log ('Though his age is 23')
+
+// using variable
+
+console.log (name + 'knows Html Css')
+console.log ('His age is ' + age)
+console.log ('He want to learn JavaScript')
+console.log ('Though his age is ' + age)
