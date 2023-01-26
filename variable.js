2b925ef7964f41182ae865087a2323c2c68c39d0
minhajul-abedin <you@example.com>
2023-01-26 16:47:53 +0600

    variable declaration

---
+++ variable.js
@@ -0,0 +1,14 @@
+// Declaration Variable 
+
+// Number variable declaration
+var age = 25;
+var weight = 65;
+
+// String variable declaration
+var name = "Minhajul Abedin"
+var surname = 'Toyon'
+
+// Boolean variable declaration
+var eat = true;
+var single = false;
+
