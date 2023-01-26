3e44e932975c905071724c776af16b0b75312af4
minhajul-abedin <you@example.com>
2023-01-26 17:24:37 +0600

    Four Operations practice

---
+++ operation.js
@@ -0,0 +1,28 @@
+//  Addition operation
+
+var orange = 40;
+var apple = 50;
+var totalFruits = orange + apple;
+
+// console.log (totalFruits);
+
+
+// Subtraction operation
+
+var apple = 100;
+var orange = 49;
+
+var Subtraction = totalFruits - orange;
+
+// console.log(Subtraction)
+
+//  Multiplies operation
+
+var Multiplies = Subtraction * apple;
+
+// console.log(Multiplies);
+
+//  division operation
+
+var division = Multiplies / apple;
+console.log(division);
