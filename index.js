382f668536e8ba9982ce18d19f4179fb938eb0bd
minhajul-abedin <you@example.com>
2023-01-28 01:15:56 +0600

    index file practice with js

---
+++ index.js
@@ -0,0 +1,15 @@
+// define array
+
+var numbers = [12, 22, 33, 34, 535, 74];
+
+// console.log(numbers);
+
+// change the numbers position
+
+// numbers [2]= 66;
+
+
+
+console.log(numbers [2]);
+
+
---
+++ shorthand.js
@@ -0,0 +1,18 @@
+var age = 25;
+age = 20;
+
+// console.log(age);
+
+// var age = age + 5;
+
+// var age = age + 10;
+// var year = 20;
+// var year += 10;
+
+// console.log(year);
+
+
+
+var num = 30;
+num++;
+console.log(num);
