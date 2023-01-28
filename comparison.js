0714530df9582ce3d0ba7e1ed818dc75e39d8987
minhajul-abedin <you@example.com>
2023-01-28 13:15:21 +0600

    comparison syntax

---
+++ comparison.js
@@ -0,0 +1,15 @@
+var a = 30;
+var b = 40;
+
+
+console.log(a > b);
+console.log(a < b);
+console.log(a == b);
+
+
+console.log(a != b);
+var c =8;
+var d =7;
+console.log(c <= d);
+
+console.log( c>=d);
