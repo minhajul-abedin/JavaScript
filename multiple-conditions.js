582142f3ae129ec303dbdd4e4adcf1eec3b6c95d
minhajul-abedin <you@example.com>
2023-01-28 14:59:35 +0600

    multiple condition practice

---
+++ multiple-conditions.js
@@ -0,0 +1,25 @@
+// var isGraduted = true;
+// var salary = 50000;
+// var car= 0;
+
+// if(isGraduted == true && salary > 500 && car == 1){
+//     console.log('let get married');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+
+
+var isGraduted = true;
+var salary = 50000;
+var car= 0;
+
+if((isGraduted == true || salary > 3000000) || car == 3) {
+    console.log('ok get married');
+}
+else{
+    console.log('hurrr');
+}
+
+
+
